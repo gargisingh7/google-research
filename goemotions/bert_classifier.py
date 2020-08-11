1a7f1f34b38c141dfdbc49918d2ba388392fc0b1
singhgargi <40858250+singhgargi@users.noreply.github.com>
2020-08-11 21:48:20 +0530

    Update bert_classifier.py

--- goemotions/bert_classifier.py
+++ goemotions/bert_classifier.py
@@ -47,9 +47,9 @@ import collections
 import json
 import os
 
-from dataset_analysis.bert import modeling
-from dataset_analysis.bert import optimization
-from dataset_analysis.bert import tokenization
+from bert import modeling
+from bert import optimization
+from bert import tokenization
 import numpy as np
 import pandas as pd
 import tensorflow as tf
